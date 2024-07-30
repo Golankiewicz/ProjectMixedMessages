796b9e56c04a960dd7d89d1a7ef4dc43b811003b
Golankiewicz <pgolankiewicz@komster.net>
2024-07-30 13:27:50 +0200

    added intro array

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //This is a fortune teller program for Codecademy JS course
+const intro = ["I see", "Stars say", "My crystal ball says"];
  const subject = ["You", 'Somebody','They'];
  const verb = ['come','go','hurt'];
- const object = ['here','there',];
+ const object = ['here','there'];
+
+ const pickingFunction = (arr)=>{
+
+ }
  
