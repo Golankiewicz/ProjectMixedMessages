6c73cf4d58d973099cd98bf0ccda049c5860c10a
Golankiewicz <pgolankiewicz@komster.net>
2024-07-31 12:31:03 +0200

    simple working version

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //This is a fortune teller program for Codecademy JS course
 const intro = ["I see", "Stars say", "My crystal ball says"];
- const subject = ["You", 'Somebody','They'];
+ const subject = ["you", 'somebody','they'];
  const verb = ['come','go','hurt'];
  const object = ['here','there'];
 
  const pickingFunction = (arr)=>{
-
+let chosenIndex = Math.floor(Math.random()*arr.length);
+return arr[chosenIndex];
  }
+ console.log(`${pickingFunction(intro)} ${pickingFunction(subject)} will ${pickingFunction(verb)} ${pickingFunction(object)}`)
  
